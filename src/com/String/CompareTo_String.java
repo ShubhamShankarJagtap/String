43e90c4722596d6e35c694b0c3d0e72dbc82facf
shubham <shubh.jagtap14@gmail.com>
2023-10-09 19:44:19 +0530

    This is for new updated comparison.

--- src/com/String/CompareTo_String.java
+++ src/com/String/CompareTo_String.java
@@ -2,7 +2,9 @@ package com.String;
 
 public class CompareTo_String {
     int comparison;
-
+    {
+        System.out.println(comparison);
+    }
     CompareTo_String(String s,String s1){
         comparison = s.compareTo(s1);
         System.out.printf("The unicode difference between these two strings is " + comparison);
@@ -11,6 +13,7 @@ public class CompareTo_String {
         String s = "sHUBHAM";
         String s1 = "jAGTAP";
         new CompareTo_String(s,s1);
+
 
     }
 }
