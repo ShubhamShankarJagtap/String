52ae35b368320ae014ea75b59bf2ab30bd1578e6
shubham <shubh.jagtap14@gmail.com>
2023-10-21 19:50:16 +0530

    This is for newly updated method learnSplit  in Split_String.java class.

--- src/com/String/Split_String.java
+++ src/com/String/Split_String.java
@@ -1,6 +1,16 @@
 package com.String;
 
 public class Split_String {
+
+ static    private void learnSplit(){
+
+     String[] forSplit = "Name=Shubham,Designation=QA,Age=27".split(",");
+
+     for (String d: forSplit){
+
+         System.out.println(d);
+     }
+    }
     public static void main(String[] args) {
 
         String s = "JAVA IS AWESOME";
@@ -11,5 +21,7 @@ public class Split_String {
 
             System.out.println(n);
         }
+
+        learnSplit();
     }
 }
